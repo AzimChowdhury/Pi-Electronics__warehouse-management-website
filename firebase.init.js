0c632af7c6db610ee6f85213fad9daef57a42a05
Azim Chowdhury <azimchy994@gmail.com>
2022-04-30 22:24:00 +0600

    environment variable added

--- firebase.init.js
+++ firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCPM0pE0KhgLKhKPIF4JHohDbx3nVDXyzU",
-    authDomain: "pi-electronics.firebaseapp.com",
-    projectId: "pi-electronics",
-    storageBucket: "pi-electronics.appspot.com",
-    messagingSenderId: "584447484658",
-    appId: "1:584447484658:web:cf201e2db7801e8057266b"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
