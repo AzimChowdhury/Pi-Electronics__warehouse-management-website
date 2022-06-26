d9f0bf0dfba1e1be843690b76f02a72b90d1b42a
Azim Chowdhury <azimchy994@gmail.com>
2022-06-26 19:40:40 +0600

    d

--- src/Custom hooks/useProducts.js
+++ src/Custom hooks/useProducts.js
@@ -3,7 +3,13 @@ import { useEffect, useState } from "react";
 const useProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('https://pi-electronics-2244.herokuapp.com/products')
+        fetch('https://pi-electronics-2244.herokuapp.com/products',{
+            method:"GET",
+            mode:"cors",
+            headers:{
+                'content-type':'application/json'
+            }
+        })
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
